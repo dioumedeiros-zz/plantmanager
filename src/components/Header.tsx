87a6aa8332db7640191bf693ff5880585aa6bf3d
Diou Medeiros <diou.182@gmail.com>
2021-04-25 20:50:31 -0300

    :sparkles: feat: Created Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Diou</Text>
+      </View>
+      <Image
+        style={styles.image}
+        source={{ uri: 'https://github.com/dioumedeiros.png' }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    paddingHorizontal: 20,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+  },
+  image: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+  },
+});
